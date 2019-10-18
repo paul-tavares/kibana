018db23d8f03da1e691f835725307115088bf373
restrry <restrry@gmail.com>
2019-10-18 18:39:10 +0200

    fix tests

--- src/core/server/http/http_server.ts
+++ src/core/server/http/http_server.ts
@@ -269,7 +269,7 @@ export class HttpServer {
     }
 
     this.registerOnPreAuth((request, response, toolkit) => {
-      const oldUrl = request.url.href!;
+      const oldUrl = request.url.pathname!;
       const newURL = basePathService.remove(oldUrl);
       const shouldRedirect = newURL !== oldUrl;
       if (shouldRedirect) {
