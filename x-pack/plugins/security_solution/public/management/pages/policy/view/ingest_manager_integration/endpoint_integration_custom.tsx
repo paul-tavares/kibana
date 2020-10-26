1f2b9b9b2ccce7ec99404f0bb6c424fb22753f2a
Paul Tavares <paul.tavares@elastic.co>
2020-10-26 16:15:31 -0400

    Show custom view for Endpoint integration

--- x-pack/plugins/security_solution/public/management/pages/policy/view/ingest_manager_integration/endpoint_integration_custom.tsx
+++ x-pack/plugins/security_solution/public/management/pages/policy/view/ingest_manager_integration/endpoint_integration_custom.tsx
@@ -4,16 +4,39 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
 import { EuiPanel, EuiSpacer, EuiText } from '@elastic/eui';
 import { IntegrationCustomComponent } from '../../../../../../../ingest_manager/common/types/extensions';
+import { MANAGEMENT_APP_ID } from '../../../../common/constants';
+import { getTrustedAppsListPath } from '../../../../common/routing';
+import { LinkToApp } from '../../../../../common/components/endpoint/link_to_app';
+import { TrustedAppsListPageRouteState } from '../../../../../../common/endpoint/types';
 
 // FIXME:PT type is not correct
 export const EndpointIntegrationCustom: IntegrationCustomComponent = memo(() => {
+  const trustedAppRouteState = useMemo<TrustedAppsListPageRouteState>(() => {
+    return {
+      backButtonLabel: 'Back to Endpoint Integration',
+      // FIXME:PT hard coded paths below
+      onBackButtonNavigateTo: [
+        'ingestManager',
+        { path: '#/integrations/detail/endpoint-0.16.0/custom' },
+      ],
+      backButtonUrl:
+        'http://localhost:5601/app/ingestManager#/integrations/detail/endpoint-0.16.0/custom',
+    };
+  }, []);
+
   return (
     <div>
       <EuiPanel paddingSize="l">
-        <EuiText>{'Trusted Application'}</EuiText>
+        <LinkToApp
+          appId={MANAGEMENT_APP_ID}
+          appPath={getTrustedAppsListPath()}
+          appState={trustedAppRouteState}
+        >
+          {'Trusted Application'}
+        </LinkToApp>
       </EuiPanel>
       <EuiSpacer />
       <EuiPanel paddingSize="l">
